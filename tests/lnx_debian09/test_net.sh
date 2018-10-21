a621419f5ad637e8a0bd0d30f150d062fcd6e3ec
Likhobabin Ilya <likhobabin_im@MacBook-Pro-Santiago.local>
2018-10-21 21:28:31 +1100

    Mac OS X HighSiera: create tuntap interface

--- tests/lnx_debian09/test_net.sh
+++ tests/lnx_debian09/test_net.sh
@@ -1,13 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
 set -o errexit
+# set -o xtrace
 set -o nounset
-set -o xtrace
 
-source ../../libs/lnx_debian09/net.sh
+ROOT_DIR_ROLL_UP_IT="/Users/likhobabin_im/Workspace/Sys/rollUpIt.lnx"
+FREERADIUS_ROOT_DIR="/etc/freeradius/3.0"
+MYSQL_MODSCFG_DIR="$FREERADIUS_ROOT_DIR/mods-config/sql/main/mysql"
+
+source "$ROOT_DIR_ROLL_UP_IT/libs/lnx_debian09/net.sh"
 
 function main() {
-    flushNetwork_NET_RUI
+declare -r debug_prefix="debug: [$0] [ $FUNCNAME[0] ] : "
+printf "$debug_prefix ENTER the function \n"
+
+	declare -r username="likhobabin_im"
+	declare -r net_dev_path="/dev/tap2"
+	declare -r interface_name="tap2"
+	declare -r network_addr="10.10.8.13/26"
+
+    createInterface_NET_RUI $username $net_dev_path $interface_name $network_addr 
+
+printf "$debug_prefix WAIT the function \n"
+    
 }
 
-main
+main $@ 
