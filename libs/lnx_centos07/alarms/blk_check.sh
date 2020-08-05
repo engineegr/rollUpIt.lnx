3e753d8219bd2f6b24608fdfa6f424061302f425
Gonzo-soc <gonzo.soc@gmail.com>
2020-08-06 02:02:32 +1100

    Add alarms for cron (memory)

---
+++ libs/lnx_centos07/alarms/blk_check.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+THRESHOLD='50'
+
+alarm_msg="$(df -hl -Bg | awk -v th="$THRESHOLD" ' \
+  { \
+    if (FNR > 1) { \
+      sub(/\%/,"",$5); \
+      if ($5 > th) { \
+        blk_map[$1] = $3 "/" $2 " " $5 "%"; \
+      } \
+    } \
+  } \
+  END { \
+  for (b in blk_map) \
+    { print b " : " blk_map[b]; } \
+    }')"
+
+if [ -n "${alarm_msg}" ]; then
+  echo -e "[Alarm] $(hostname) Storage space is exhausting (less than $THRESHOLD %).\n$alarm_msg"
+else
+  echo -e "[INFO] $(hostname) Storage space is OK"
+fi
---
+++ libs/lnx_centos07/alarms/ram_check.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+THRESHOLD='10'
+
+alarm_msg="$(free -t -hl | awk -v th=$THRESHOLD '/Total/{ sub("M", "", $4); if (int($4) < 100) { print "[Alarm] Free RAM: " $4 " [M] less than " th " [M]"; } }')"
+
+if [ -n "${alarm_msg}" ]; then
+  while IFS= read -r u; do
+    username="$(echo $u | awk '{print $1}')"
+    t="$(echo $u | awk '{print $2}')"
+    echo "u: $u"
+    if [ -n "$(echo $u | grep -E '(gonzo|root)')" ]; then
+      echo "${alarm_msg}" | write $username $t
+    fi
+  done < <(who)
+
+  echo "$(hostname) MSG ${alarm_msg}"
+fi
